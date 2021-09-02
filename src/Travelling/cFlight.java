49685764875b4dabbda32ea36e22528815ee9948
unknown <rshetul4@gmail.com>
2021-09-03 01:33:16 +0600

    first commit

---
+++ src/Travelling/cFlight.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Travelling;
+
+/**
+ *
+ * @author Rezwan
+ */
+class cFlight {
+    
+    
+    public double      Cox = 4850;
+    public double      St_Martin = 5150;
+    public double      Nilgiri = 6550;
+    public double      Sundarbans_1 = 3550;
+    public double      Sundarbans_3 = 4850;
+    public double      Sylhet_R_B = 4750;
+    public double      Sylhet_Jaflong = 4450;
+    public double      Sylhet_All = 9500;
+    
+    
+    
+}
